cdff1be3d1787521efa7fb585bb65c45ae2a661d
Youssef Amazzal <20youssefyoussef02@gmail.com>
2025-05-19 08:58:56 +0100

    initial commit

---
+++ src/main/java/amazzal/youssef/examjee/ExamJeeApplication.java
@@ -0,0 +1,13 @@
+package amazzal.youssef.examjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamJeeApplication.class, args);
+    }
+
+}
